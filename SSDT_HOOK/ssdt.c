713c70d202a7c23f6957f52f964191598c2f5478
奕雨 阳 <wakappxc@outlook.com>
2026-03-22 18:55:11 +0800

    Revert "202603225"
    This reverts commit 9120a9264d2d8ec3ade6c4f5d8d0d452eb8c8a31.

--- SSDT_HOOK/ssdt.c
+++ SSDT_HOOK/ssdt.c
@@ -12,7 +12,7 @@ ULONG wpOff()
 {
 	ULONG cr0 = __readcr0();
 	_disable();
-	__writecr0(cr0 & (~0x));
+	__writecr0(cr0 & (~0x10000));
 	return cr0;
 }
 
@@ -40,24 +40,23 @@ VOID SsdtDestory()
 
 PSsdtTable SsdtGet()
 {
-	return (PSsdtTable)((PUCHAR)KeServiceDescriptorTable + 0x);
+	return (PSsdtTable)((PUCHAR)KeServiceDescriptorTable + 0x40);
 }
 
-ULONG SsdtGetFunctionIndex(char * funName)
+ULONG SsdtGetFunctionIndex(char* funName)
 {
 	PUCHAR func = (PUCHAR)ExportTableFuncByName(gMapNtdll, funName);
-	if (!func) return ;
+	if (!func) return -1;
 	return *(PULONG)(func + 1);
 }
-//ÄãºÃ11
-//°¡1
-ULONG_PTR SsdtSetHook(char * funName, ULONG_PTR newFunction)
+
+ULONG_PTR SsdtSetHook(char* funName, ULONG_PTR newFunction)
 {
 	PSsdtTable ssdtTable = SsdtGet();
 
 	ULONG index = SsdtGetFunctionIndex(funName);
 
-	if (index == ) return 0;
+	if (index == -1) return 0;
 
 	ULONG function = ssdtTable->ssdt.funcTable[index];
 
@@ -70,3 +69,4 @@ ULONG_PTR SsdtSetHook(char * funName, ULONG_PTR newFunction)
 	return function;
 
 }
+
